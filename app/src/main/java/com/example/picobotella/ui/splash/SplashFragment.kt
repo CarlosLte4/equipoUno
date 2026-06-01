a8aab5dca20d002de6e05fae830e8d9732b107d0
es-sai-fi <athousandheavenlycats@protonmail.com>
2026-05-31 20:49:16 -0500

    refactor: HomeFragment
    - Simplified logic related to spinning and bottle button
    - Simplified sound logic and abstracted useful functions

--- app/src/main/java/com/example/picobotella/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/picobotella/ui/splash/SplashFragment.kt
@@ -1,8 +1,6 @@
 package com.example.picobotella.ui.splash
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,34 +12,31 @@ import com.example.picobotella.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
 
-    lateinit var binding: FragmentSplashBinding
+  lateinit var binding: FragmentSplashBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_splash, container, false)
-        return binding.root
-    }
+  override fun onCreateView(
+      inflater: LayoutInflater,
+      container: ViewGroup?,
+      savedInstanceState: Bundle?,
+  ): View {
+    binding = DataBindingUtil.inflate(inflater, R.layout.fragment_splash, container, false)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    return binding.root
+  }
 
-        // Animación de la botella
-        binding.bottleIcon.animate()
-            .rotation(360f)
-            .setDuration(2000)
-            .withEndAction {
-                binding.bottleIcon.animate().rotation(720f).setDuration(2000).start()
-            }
-            .start()
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
 
-        // Navegar al Home después de 5 segundos
-        Handler(Looper.getMainLooper()).postDelayed({
-            if (isAdded) {
-                findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
-            }
-        }, 5000)
-    }
+    binding.bottleIcon.animate().rotation(360f).setDuration(2000).start()
+
+    view.postDelayed(
+        {
+          if (isAdded) {
+
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+          }
+        },
+        5000,
+    )
+  }
 }
